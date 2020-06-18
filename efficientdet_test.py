715b77d816cc2d415aa63d44dc92e7f33a38e9d2
gengxqa <850966964@qq.com>
2020-06-18 08:11:38 +0800

    调通代码

--- efficientdet_test.py
+++ efficientdet_test.py
@@ -11,74 +11,83 @@ from matplotlib import colors
 from backbone import EfficientDetBackbone
 import cv2
 import numpy as np
-
+import os
+import glob
 from efficientdet.utils import BBoxTransform, ClipBoxes
 from utils.utils import preprocess, invert_affine, postprocess, STANDARD_COLORS, standard_to_bgr, get_index_label, plot_one_box
 
-compound_coef = 0
+compound_coef = 4
 force_input_size = None  # set None to use default size
-img_path = 'test/img.png'
+# img_path = ['test/img.png']
+
 
 # replace this part with your project's anchor config
-anchor_ratios = [(1.0, 1.0), (1.4, 0.7), (0.7, 1.4)]
+anchor_ratios = [(0.9, 1.1), (1.0, 1.0), (1.1, 0.9)]
 anchor_scales = [2 ** 0, 2 ** (1.0 / 3.0), 2 ** (2.0 / 3.0)]
 
-threshold = 0.2
-iou_threshold = 0.2
+threshold = 0.2       #rpn置信度阈值
+iou_threshold = 0.2   #nms阈值
 
 use_cuda = True
 use_float16 = False
 cudnn.fastest = True
 cudnn.benchmark = True
 
-obj_list = ['person', 'bicycle', 'car', 'motorcycle', 'airplane', 'bus', 'train', 'truck', 'boat', 'traffic light',
-            'fire hydrant', '', 'stop sign', 'parking meter', 'bench', 'bird', 'cat', 'dog', 'horse', 'sheep',
-            'cow', 'elephant', 'bear', 'zebra', 'giraffe', '', 'backpack', 'umbrella', '', '', 'handbag', 'tie',
-            'suitcase', 'frisbee', 'skis', 'snowboard', 'sports ball', 'kite', 'baseball bat', 'baseball glove',
-            'skateboard', 'surfboard', 'tennis racket', 'bottle', '', 'wine glass', 'cup', 'fork', 'knife', 'spoon',
-            'bowl', 'banana', 'apple', 'sandwich', 'orange', 'broccoli', 'carrot', 'hot dog', 'pizza', 'donut',
-            'cake', 'chair', 'couch', 'potted plant', 'bed', '', 'dining table', '', '', 'toilet', '', 'tv',
-            'laptop', 'mouse', 'remote', 'keyboard', 'cell phone', 'microwave', 'oven', 'toaster', 'sink',
-            'refrigerator', '', 'book', 'clock', 'vase', 'scissors', 'teddy bear', 'hair drier',
-            'toothbrush']
+# obj_list = ['person', 'bicycle', 'car', 'motorcycle', 'airplane', 'bus', 'train', 'truck', 'boat', 'traffic light',
+#             'fire hydrant', '', 'stop sign', 'parking meter', 'bench', 'bird', 'cat', 'dog', 'horse', 'sheep',
+#             'cow', 'elephant', 'bear', 'zebra', 'giraffe', '', 'backpack', 'umbrella', '', '', 'handbag', 'tie',
+#             'suitcase', 'frisbee', 'skis', 'snowboard', 'sports ball', 'kite', 'baseball bat', 'baseball glove',
+#             'skateboard', 'surfboard', 'tennis racket', 'bottle', '', 'wine glass', 'cup', 'fork', 'knife', 'spoon',
+#             'bowl', 'banana', 'apple', 'sandwich', 'orange', 'broccoli', 'carrot', 'hot dog', 'pizza', 'donut',
+#             'cake', 'chair', 'couch', 'potted plant', 'bed', '', 'dining table', '', '', 'toilet', '', 'tv',
+#             'laptop', 'mouse', 'remote', 'keyboard', 'cell phone', 'microwave', 'oven', 'toaster', 'sink',
+#             'refrigerator', '', 'book', 'clock', 'vase', 'scissors', 'teddy bear', 'hair drier',
+#             'toothbrush']
+obj_list = ['positive', 'negative', 'stromal', 'lymphocyte', 'normal']
 
 
 color_list = standard_to_bgr(STANDARD_COLORS)
 # tf bilinear interpolation is different from any other's, just make do
 input_sizes = [512, 640, 768, 896, 1024, 1280, 1280, 1536]
 input_size = input_sizes[compound_coef] if force_input_size is None else force_input_size
-ori_imgs, framed_imgs, framed_metas = preprocess(img_path, max_size=input_size)
 
-if use_cuda:
-    x = torch.stack([torch.from_numpy(fi).cuda() for fi in framed_imgs], 0)
-else:
-    x = torch.stack([torch.from_numpy(fi) for fi in framed_imgs], 0)
+def main(img_path, base_name, checkpoint_path):
+    ori_imgs, framed_imgs, framed_metas = preprocess(img_path, max_size=input_size)
 
-x = x.to(torch.float32 if not use_float16 else torch.float16).permute(0, 3, 1, 2)
+    if use_cuda:
+        x = torch.stack([torch.from_numpy(fi).cuda() for fi in framed_imgs], 0)
+    else:
+        x = torch.stack([torch.from_numpy(fi) for fi in framed_imgs], 0)
 
-model = EfficientDetBackbone(compound_coef=compound_coef, num_classes=len(obj_list),
-                             ratios=anchor_ratios, scales=anchor_scales)
-model.load_state_dict(torch.load(f'weights/efficientdet-d{compound_coef}.pth'))
-model.requires_grad_(False)
-model.eval()
+    x = x.to(torch.float32 if not use_float16 else torch.float16).permute(0, 3, 1, 2)
 
-if use_cuda:
-    model = model.cuda()
-if use_float16:
-    model = model.half()
+    model = EfficientDetBackbone(compound_coef=compound_coef, num_classes=len(obj_list),
+                                ratios=anchor_ratios, scales=anchor_scales)
+    # model.load_state_dict(torch.load(f'weights/efficientdet-d{compound_coef}.pth'))
+    model.load_state_dict(torch.load(checkpoint_path))
+    model.requires_grad_(False)
+    model.eval()
 
-with torch.no_grad():
-    features, regression, classification, anchors = model(x)
+    if use_cuda:
+        model = model.cuda()
+    if use_float16:
+        model = model.half()
 
-    regressBoxes = BBoxTransform()
-    clipBoxes = ClipBoxes()
+    with torch.no_grad():
+        features, regression, classification, anchors = model(x)
 
-    out = postprocess(x,
-                      anchors, regression, classification,
-                      regressBoxes, clipBoxes,
-                      threshold, iou_threshold)
+        regressBoxes = BBoxTransform()
+        clipBoxes = ClipBoxes()
 
-def display(preds, imgs, imshow=True, imwrite=False):
+        out = postprocess(x,
+                        anchors, regression, classification,
+                        regressBoxes, clipBoxes,
+                        threshold, iou_threshold)
+
+    out = invert_affine(framed_metas, out)
+    display(out, ori_imgs, base_name,imshow=False, imwrite=True)
+    
+def display(preds, imgs, base_name, imshow=True, imwrite=False):
     for i in range(len(imgs)):
         if len(preds[i]['rois']) == 0:
             continue
@@ -95,29 +104,28 @@ def display(preds, imgs, imshow=True, imwrite=False):
             cv2.waitKey(0)
 
         if imwrite:
-            cv2.imwrite(f'test/img_inferred_d{compound_coef}_this_repo_{i}.jpg', imgs[i])
+            cv2.imwrite(f'test003/img_inferred_d{compound_coef}_{base_name}.jpg', imgs[i])
 
 
-out = invert_affine(framed_metas, out)
-display(out, ori_imgs, imshow=False, imwrite=True)
 
-print('running speed test...')
-with torch.no_grad():
-    print('test1: model inferring and postprocessing')
-    print('inferring image for 10 times...')
-    t1 = time.time()
-    for _ in range(10):
-        _, regression, classification, anchors = model(x)
 
-        out = postprocess(x,
-                          anchors, regression, classification,
-                          regressBoxes, clipBoxes,
-                          threshold, iou_threshold)
-        out = invert_affine(framed_metas, out)
+# print('running speed test...')
+# with torch.no_grad():
+#     print('test1: model inferring and postprocessing')
+#     print('inferring image for 10 times...')
+#     t1 = time.time()
+#     for _ in range(10):
+#         _, regression, classification, anchors = model(x)
 
-    t2 = time.time()
-    tact_time = (t2 - t1) / 10
-    print(f'{tact_time} seconds, {1 / tact_time} FPS, @batch_size 1')
+#         out = postprocess(x,
+#                           anchors, regression, classification,
+#                           regressBoxes, clipBoxes,
+#                           threshold, iou_threshold)
+#         out = invert_affine(framed_metas, out)
+
+#     t2 = time.time()
+#     tact_time = (t2 - t1) / 10
+#     print(f'{tact_time} seconds, {1 / tact_time} FPS, @batch_size 1')
 
     # uncomment this if you want a extreme fps test
     # print('test2: model inferring only')
@@ -130,3 +138,13 @@ with torch.no_grad():
     # t2 = time.time()
     # tact_time = (t2 - t1) / 10
     # print(f'{tact_time} seconds, {32 / tact_time} FPS, @batch_size 32')
+
+if __name__ == '__main__':
+    checkpoint_path = '/home/gengxiaoqi/Yet-Another-EfficientDet-Pytorch/logs/COCO_ki67_NEC/efficientdet-d4_246_48000_003.pth'
+    img_path = '/mnt/share/quantitative_analysis_database/COCO_ki67_NEC/val/*.jpg'
+    img_path = glob.glob(img_path)
+    # img_path = ['/mnt/share/quantitative_analysis_database/COCO_ki67_NEC/val/patch_919347-2 KI67 - 2019-11-15 12.29.47.ndpi_ul_x_6170_ul_y_31286.jpg']
+    for i in img_path:
+        img = [i]
+        base_name = i.split('/')[-1]
+        main(img,base_name,checkpoint_path)
